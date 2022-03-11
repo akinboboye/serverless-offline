060720d6c016ea33fb4177e2376caed8cbdd4e35
Shina Akinboboye <shina.akinboboye@hotelengine.com>
2022-03-11 15:33:46 -0500

    testing state

--- examples/tools/babel-register/src/handler.js
+++
@@ -1,8 +0,0 @@
-const { stringify } = JSON
-
-export async function hello() {
-  return {
-    body: stringify({ hello: '@babel/register' }),
-    statusCode: 200,
-  }
-}
--- examples/tools/ts-node/src/bridge.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-require('ts-node').register()
-module.exports = require('./handler.ts')
